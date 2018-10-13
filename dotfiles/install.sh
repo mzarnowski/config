c9bce6e9ad9e20b6e34e971b66f4d1af3eae669c
Marek Żarnowski <marek1840@gmail.com>
2018-10-13 15:54:44 +0200

    use declared path for dotfiles directory

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -1,13 +1,13 @@
 dotfiles="$(dirname "$(readlink -f $0)")"
 
-ln -s dotfiles/.config/i3 ~/.config/i3
-ln -s dotfiles/.config/ranger ~/.config/ranger
-ln -s dotfiles/.config/dunst ~/.config/dunst
-ln -s dotfiles/.config/chrome-flags.conf ~/.config/chrome-flags.conf
+ln -s "${dotfiles}"/.config/i3 ~/.config/i3
+ln -s "${dotfiles}"/.config/ranger ~/.config/ranger
+ln -s "${dotfiles}"/.config/dunst ~/.config/dunst
+ln -s "${dotfiles}"/.config/chrome-flags.conf ~/.config/chrome-flags.conf
 
 
-ln -s dotfiles/.i3blocks.conf ~/.i3blocks.conf
-ln -s dotfiles/.Xresources ~/.Xresources
-ln -s dotfiles/.zshrc ~/.zshrc
-ln -s dotfiles/.vimhrc ~/.vimhrc
-ln -s dotfiles/.gitconfig ~/.gitconfig
+ln -s "${dotfiles}"/.i3blocks.conf ~/.i3blocks.conf
+ln -s "${dotfiles}"/.Xresources ~/.Xresources
+ln -s "${dotfiles}"/.zshrc ~/.zshrc
+ln -s "${dotfiles}"/.vimrc ~/.vimrc
+ln -s "${dotfiles}"/.gitconfig ~/.gitconfig
--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 dir="$(dirname "$(readlink -f $0)")"
 dotfiles="${dir}/dotfiles"
 
-ln -s dotfiles/.yaourtrc       ~/.yaourtrc
+ln -s "${dotfiles}"/.yaourtrc       ~/.yaourtrc
 
 ./install_from_community.sh
 ./install_from_aur.sh
