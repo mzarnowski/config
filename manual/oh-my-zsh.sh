23350aef2c38fbf604abf036894b9142e80d077e
Marek Żarnowski <marek1840@gmail.com>
2018-10-13 15:59:44 +0200

    auto install k plugin for oh-my-zsh

--- manual/oh-my-zsh.sh
+++ manual/oh-my-zsh.sh
@@ -1,2 +1,6 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# install k
+git clone https://github.com/supercrabtree/k $ZSH_CUSTOM/plugins/k
+
+
