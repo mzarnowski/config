62b455080a3a5a6459d9af0367b525d4c295d997
Marek Żarnowski <mzarnowski@virtuslab.com>
2019-01-21 13:38:07 +0100

    add startup scripts

---
+++ dotfiles/.config/bash/startup/configure_screen.sh
@@ -0,0 +1,5 @@
+LEFT="DP2-1"
+RIGHT="DP2-2"
+
+# xrandr --auto --output "${LEFT}" --mode 1920x1080 --left-of "${RIGHT}" && \
+#   xrandr --auto --output "${RIGHT}" --mode  1920x1080 --right-of "${LEFT}"
