983880c98c9273fe5dcd51c6efa7c98012687038
Marek Żarnowski <mzarnowski@virtuslab.com>
2019-01-21 13:38:56 +0100

    add fzf kill and backup/restore functions

--- dotfiles/.config/bash/functions.sh
+++ dotfiles/.config/bash/functions.sh
@@ -2,3 +2,27 @@ take(){
     local TARGET_PATH=$1
     mkdir -p "${TARGET_PATH}" && cd "${TARGET_PATH}"
 }
+
+fkill() {
+    local pid
+    if [ "$UID" != "0" ]; then
+        pid=$(ps -f -u $UID | sed 1d | fzf -m | awk '{print $2}')
+    else
+        pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
+    fi
+
+    if [ "x$pid" != "x" ]
+    then
+        echo $pid | xargs kill -${1:-9}
+    fi
+}
+
+backup(){
+    local target=$1
+    mv "${target}"{,.bak}
+}
+
+restore(){
+    local target=$1
+    mv "${target}"{.bak,}
+}
