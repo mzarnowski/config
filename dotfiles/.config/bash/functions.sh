7b9a5fb34521c12fc5cfb88f6d5ec348e8945bb0
Marek Żarnowski <mzarnowski@virtuslab.com>
2019-01-29 10:47:18 +0100

    use fzf for kill completion

--- dotfiles/.config/bash/functions.sh
+++ dotfiles/.config/bash/functions.sh
@@ -15,20 +15,6 @@ confirm(){
     esac
 }
 
-fkill() {
-    local pid
-    if [ "$UID" != "0" ]; then
-        pid=$(ps -f -u $UID | sed 1d | fzf -m | awk '{print $2}')
-    else
-        pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
-    fi
-
-    if [ "x$pid" != "x" ]
-    then
-        echo $pid | xargs kill -${1:-9}
-    fi
-}
-
 backup(){
     local target=$1
     mv "${target}"{,.bak}
