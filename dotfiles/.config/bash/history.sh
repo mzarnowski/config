37b448b6d7e6202ab2a0c8839800404369a4fd91
Marek Żarnowski <marek1840@gmail.com>
2020-04-20 11:56:51 +0200

    Record history immediately instead of after shell exit

--- dotfiles/.config/bash/history.sh
+++ dotfiles/.config/bash/history.sh
@@ -3,6 +3,8 @@
 shopt -s histappend # Append to the history file, don't overwrite it
 shopt -s cmdhist # Save multi-line commands as one command
 
+export PROMPT_COMMAND="history -a; $PROMPT_COMMAND" # Append immediately, not after shell exits
+export HISTTIMEFORMAT="[%F %T] "
 export HISTCONTROL="erasedups:ignoreboth" # Avoid duplicate entries
 export HISTSIZE=50000
 export HISTFILESIZE=50000
