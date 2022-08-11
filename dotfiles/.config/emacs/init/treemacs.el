355540d30b08f5ff5e472df6603eff38def44be0
Marek Żarnowski <marek1840@gmail.com>
2022-08-11 22:51:43 +0200

    Simplify opening and closing treemacs window

--- dotfiles/.config/emacs/init/treemacs.el
+++ dotfiles/.config/emacs/init/treemacs.el
@@ -1,17 +1,7 @@
-(defun my/treemacs-go-to-window ()
-  (let* ((focused-on-tree-buffer (equal (treemacs-get-local-buffer  ) (window-buffer)))
-	 (is-tree-buffer-visible (equal (treemacs-current-visibility) 'visible)))
-    (cond (focused-on-tree-buffer (treemacs))
-	  (is-tree-buffer-visible (treemacs-select-window))
-	  (t                      (treemacs-select-window)))))
-
 (use-package treemacs
   :defer t
   :config
   (init/disable-line-numbers 'treemacs-mode-hook)
-  :bind
-  (:map global-map
-	("M-1" . (lambda () (interactive) (my/treemacs-go-to-window)))))
+  (setq treemacs-select-when-already-in-treemacs 'close)
+  (general-define-key "M-1"    'treemacs-select-window))
 
-
-      
