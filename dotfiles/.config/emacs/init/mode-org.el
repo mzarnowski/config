64f96aec7662f663f9ccdf04f5c1b14a426114c4
Marek Żarnowski <marek1840@gmail.com>
2022-08-12 23:25:07 +0200

    Configure org-mode

--- dotfiles/.config/emacs/init/mode-org.el
+++ dotfiles/.config/emacs/init/mode-org.el
@@ -1,4 +1,14 @@
-(use-package org)
+(use-package org
+  :config
+  (setq org-hide-emphasis-markers      t)
+  (setq org-startup-indented           t)
+  (setq org-pretty-entities            t)
+  (setq org-hide-emphasis-markers      t)
+  (setq org-startup-with-inline-images t)
+  (use-package org-superstar
+    :config
+    (setq org-superstar-special-todo-items t)
+    (add-hook 'org-mode-hook (lambda() (org-superstar-mode 1)))))
 
 (use-package org-roam
   :custom
@@ -9,5 +19,6 @@
    ("C-c n i" . org-roam-node-insert  )
    ("C-c n l" . org-roam-buffer-toggle))
   :config
-  (setq org-return-follows-link  t)
+  (setq org-return-follows-link        t)
   (org-roam-setup))
+
