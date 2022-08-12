436f160d7599247b0face6050d927cc2fb741921
Marek Żarnowski <marek1840@gmail.com>
2022-08-12 23:24:38 +0200

    Install possibly better dark theme

--- dotfiles/.config/emacs/init/theme.el
+++ dotfiles/.config/emacs/init/theme.el
@@ -1,11 +1,12 @@
 (use-package doom-themes)
 (load-theme 'doom-one-light)
 (load-theme 'doom-palenight)
+(load-theme 'doom-1337)
 
 (defun toggle-theme ()
   (interactive)
   (let* ((day   'doom-one-light)
-	 (night 'doom-palenight)	 
+	 (night 'doom-1337)	 
 	 (is-day (eq (car custom-enabled-themes) day))
 	 (other-theme (if is-day night day)))
     (enable-theme other-theme)))
