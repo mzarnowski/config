f5656db654530f04874e477011345f73e1ad3729
Marek Żarnowski <marek1840@gmail.com>
2022-12-01 12:23:22 +0100

    Create thought-exploration major mode

--- linked/home/.config/emacs/extensions/work/mode-thought/mode-thought-explore.el
+++ linked/home/.config/emacs/extensions/work/mode-thought/mode-thought-explore.el
@@ -27,10 +27,6 @@
   (setq-local tabulated-list-entries (workspace-thoughts--explore-buffer-entries workspace mode))
   (tabulated-list-print))
 
-(cl-defun workspace-thoughts--explore-get-buffer-create (workspace mode)
-  (let ((buffer-name (format "%s: thoughts" (workspace-name workspace))))
-    (get-buffer-create buffer-name)))
-
 (cl-defun workspace-thoughts--explore-buffer-headers (workspace mode)
   [("Created" 10 t)
    ("Status" 6 t)
@@ -60,41 +56,66 @@ The format for the list element is defined by 'tabulated-list-entries':
     (let ((thoughts (workspace-thoughts--find-thoughts workspace mode :query '(todo))))
       (-non-nil (-map #'f thoughts)))))
 
-(cl-defun workspace-thoughts--explore-buffer-act-on-f (workspace mode)
-  (lambda ()
-    (interactive)
-    (let ((marker (get-text-property 0 :org-marker (tabulated-list-get-id))))
-      (with-current-buffer (marker-buffer marker)
-	(switch-to-buffer (current-buffer))))))
+(cl-defun workspace-thoughts--explore-buffer-act ()
+  (interactive)
 
-(cl-defun workspace-thoughts--explore-buffer-refresh-f (workspace mode)
-  (lambda () (workspace-thoughts--refresh-explore-buffer workspace mode)))
+  (unless (and current-workspace current-workmode)
+    (error "thoughts-explore buffer not initialized"))
 
-(cl-defun workspace-thoughts--explore-buffer (workspace mode)
-  (let* ((buffer (workspace-thoughts--explore-get-buffer-create workspace mode))
-	 (header (workspace-thoughts--explore-buffer-headers    workspace mode)))
-    (with-current-buffer buffer
-      (tabulated-list-mode)
-      (setq-local header-line-format header
-		  tabulated-list-padding  2
-		  tabulated-list-sort-key '("Created" . t))
+  (let ((marker (get-text-property 0 :org-marker (tabulated-list-get-id))))
+    (with-current-buffer (marker-buffer marker)
+      (switch-to-buffer (current-buffer)))))
 
+(cl-defun workspace-thoughts--explore-buffer-refresh ()
+  (unless (and current-workspace current-workmode)
+    (error "thoughts-explore buffer not initialized"))
+
+  (workspace-thoughts--refresh-explore-buffer current-workspace current-workmode))
+
+(defvar workspace-thoughts-explore-mode-map
+  (workspace-define-keymap
+   `("RET" "refine" workspace-thoughts--explore-buffer-act)))
+
+(define-derived-mode workspace-thoughts-explore-mode tabulated-list-mode "Thoughts"
+  (add-hook 'tabulated-list-revert-hook
+	    #'workspace-thoughts--explore-buffer-refresh
+	    nil t)
+
+  (setq-local tabulated-list-padding  2
+	      tabulated-list-sort-key '("Created" . t)))
+
+(cl-defun workspace-thoughts-explore-mode-p ()
+  (equal 'workspace-thoughts-explore-mode major-mode))
+
+(cl-defun workspace-thoughts-explore-mode-initialize-buffer (workspace mode &optional buffer)
+  (with-current-buffer (or buffer (current-buffer))
+    (unless (workspace-thoughts-explore-mode-p)
+      (error "not in workspace-thoughts-explore-mode"))
+
+    (setq-local current-workspace workspace
+		current-workmode  mode)
+
+    (let ((header (workspace-thoughts--explore-buffer-headers workspace mode)))
       (setq tabulated-list-format header)
-      (tabulated-list-init-header)
+      (tabulated-list-init-header))))
 
-      (local-set-key (kbd "RET") (workspace-thoughts--explore-buffer-act-on-f workspace mode))
-
-      (add-hook 'tabulated-list-revert-hook
-		(workspace-thoughts--explore-buffer-refresh-f workspace mode)
-		nil t)
-      (revert-buffer))
+;; TODO should be idempotent?
+(cl-defun workspace-thoughts--explore-make-buffer (workspace mode buffer-name)
+  (let ((buffer (get-buffer-create buffer-name)))
+    (with-current-buffer buffer
+      (workspace-thoughts-explore-mode)
+      (workspace-thoughts-explore-mode-initialize-buffer workspace mode))
     buffer))
 
-(cl-defun workspace-thoughts--explore (workspace mode)
-  (let* ((thoughts (workspace-thoughts--find-thoughts workspace mode :query '(todo)))
-	 (strings (-map #'org-ql-view--format-element thoughttabulated-list-modes)))
-    (org-ql-view--display
-      :buffer "*foo*"
-      :header "my header"
-      :string (s-join "\n" strings))))
+(cl-defun workspace-thoughts--explore-get-buffer-create (workspace mode)
+  (let* ((buffer-name (format "%s: thoughts" (workspace-name workspace)))
+	 (buffer (get-buffer buffer-name)))
+    (or buffer
+	(workspace-thoughts--explore-make-buffer workspace mode buffer-name))))
 
+(cl-defun workspace-thoughts--explore (workspace mode)
+  (let* ((buffer (workspace-thoughts--explore-get-buffer-create workspace mode)))
+    (with-current-buffer buffer
+      (revert-buffer))
+
+    (switch-to-buffer buffer)))
